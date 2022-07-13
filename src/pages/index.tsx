c1cde250ff2b6738d58d039696b31a0919c81e83
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-07-13 18:29:26 +0800

    docs($index): about React Hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 
 const Index: React.FC<any> = () => {
-  // 其实相当于每次重新渲染, 函数都会从从头到尾执行一遍, 只不过使用了react hooks 能够将state保存在某个地方, 避免被重新声明变量
+  // 函数式组件每次渲染, 函数都会从从头到尾执行一遍, 只不过使用了React Hooks之后, React能够将state保存在某个地方, 并且确保你能访问到, 避免被重新声明变量
   return (
     <div>Hello World</div>
   )
