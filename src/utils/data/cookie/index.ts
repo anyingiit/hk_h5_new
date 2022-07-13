3c9c0f41a0fdfbc9fcf231a1a4ff7bd046dccc4b
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-07-13 18:21:33 +0800

    feat($utils): utils structure

---
+++ src/utils/data/cookie/index.ts
@@ -0,0 +1,5 @@
+// export {cookie} from './cookie'
+// export {default as userLoginState} from './modules/login' // 导入某个模块的默认导出, 并且重命名
+
+export * from './modules/login'
+export {cookie} from "./cookie";
