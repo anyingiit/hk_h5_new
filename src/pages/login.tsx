16129b7198377e41f0212499e48fc3a8426a7a7e
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-07-15 17:35:19 +0800

    feat($readme): basic complete login and register page, and register step1 basic service logic

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,93 @@
-import React from "react";
-import {AxiosError} from "axios";
-import {patientRegister} from "../api";
+import React, {useState} from "react";
+import {AutoCenter, Button, Form, Grid, Input, Toast} from "antd-mobile";
+import {useRouter} from "next/router";
+import styles from "../css/pages/login.module.scss"
 
+// TODO: 应当抽象公共逻辑, 使得该页面患者和医生登录都共用一个页面
 const Login: React.FC<any> = () => {
-  const register = () => {
-    patientRegister("18888888889", "123456")
-      .then((resp) => {
-        console.log(resp.data)
-        console.log(resp)
-      })
-      .catch((error: AxiosError) => {
-        console.log(error)
-      })
+  const router = useRouter()
+  //TODO: 如果用户因为账号过期退出登录, 那么默认值可以是cookie中用户的手机号
+  const [telephone, setTelephone] = useState('')
+  const [password, setPassword] = useState('')
+
+  const clickLogin = () => {
+
+  }
+  const clickRegister = () => {
+    router.push({
+      pathname: '/register',
+      query: {
+        telephone: telephone
+      }
+    })
+  }
+  const clickRetrieve = () => {
+    Toast.show({
+      content: '敬请期待!',
+      maskClickable: false,
+      position: 'bottom'
+    })
   }
   return (
     <div>
-      <button onClick={register}>注册</button>
+      <AutoCenter><h2>患者登录</h2></AutoCenter>
+      <Form layout={`horizontal`} requiredMarkStyle='text-optional' className={styles.form}>
+        <Form.Item
+          name={`telephone`}
+          label={`手机号`}
+          rules={[{required: true, message: '手机号不能为空'}]}
+        >
+          <Input onChange={val => setTelephone(val)} placeholder={`请输入手机号`}/>
+        </Form.Item>
+        <Form.Item
+          name={`password`}
+          label={`密码`}
+          rules={[{required: true, message: '密码不能为空'}]}
+        >
+          <Input onChange={val => setPassword(val)} placeholder={`请输入密码`} clearable={true} type={`password`}/>
+        </Form.Item>
+      </Form>
+      {/*<AutoCenter>*/}
+      {/*  <Button */}
+      {/*    color={`primary`} */}
+      {/*    onClick={() => {*/}
+      {/*    router.push('/register')*/}
+      {/*  }}>登录*/}
+      {/*  </Button>*/}
+      {/*</AutoCenter>*/}
+      {/*TODO: 将Button移至From的字段属性的footer中, 以便可以使用其检测表单是否合法的特性*/}
+      <Button
+        className={styles.loginButton}
+        block={true}
+        size={`large`}
+        color={`primary`}
+        onClick={clickLogin}
+      >登录
+      </Button>
+      <Grid columns={2}>
+        <Grid.Item>
+          <AutoCenter>
+            <Button
+              fill={`outline`}
+              color={`primary`}
+              onClick={clickRegister}
+            >立即注册
+            </Button>
+          </AutoCenter>
+        </Grid.Item>
+        <Grid.Item>
+          <AutoCenter>
+            <Button
+              fill={`outline`}
+              color={`primary`}
+              onClick={clickRetrieve}
+            >找回密码
+            </Button>
+          </AutoCenter>
+        </Grid.Item>
+      </Grid>
+      <div>
+      </div>
     </div>
   )
 }
---
+++ src/pages/register.tsx
@@ -0,0 +1,128 @@
+import React, {useState} from "react";
+import {patientRegister} from "../api";
+import {AxiosError} from "axios";
+import {Button, Form, Input, Steps, Toast} from "antd-mobile";
+import {useRouter} from "next/router";
+
+const {Step} = Steps
+
+interface Query {
+  /**
+   * 用户是否已经完成基本账号信息的注册, 如果已完成那么直接将注册阶段跳到`主要信息`阶段
+   */
+  isCompleteAccountRegister?: boolean,
+  /**
+   * 用户如果在其他对方输入过手机号(例如登录页), 那么可以传入该参数, 免得用户再次输入
+   */
+  telephone?: string
+}
+
+// TODO: props应当能够接受一个参数, 标识用户已经完成了账号注册, 但是需要继续进行必要信息的填写
+// TODO: props应当能接受一个手机号, 该手机号的作用就是用户在登录界面输入了手机号, 然后点击了注册, 此时将该手机号直接输入到该页面手机号框内, 免得用户重复输入手机号
+/**
+ * 用户注册页面
+ * 用户注册共计三个阶段
+ *    1. 账号信息
+ *    2. 主要信息
+ *    3. 其他信息
+ */
+const Register: React.FC<any> = () => {
+  const router = useRouter()
+  const {isCompleteAccountRegister, telephone: queryTelephone}: Query = router.query
+  console.log(isCompleteAccountRegister)
+  console.log(queryTelephone)
+
+  //TODO: 如果用户因为账号过期退出登录, 那么默认值可以是cookie中用户的手机号
+  //TODO: 应当将第一页注册表单单独抽象为一个页面, 其中telephone, password, conformPassword由该页面维护
+  const [telephone, setTelephone] = useState(queryTelephone ?? '')
+  const [password, setPassword] = useState('')
+  const [conformPassword, setConformPassword] = useState('')
+
+  //TODO: 到底是使用路由切换页面, 还是只用render刷新不同的组件?
+  const [curStep, setCurStep] = useState(0)
+  const clickNextStep = () => {
+    switch (curStep) {
+      case 0:
+        // TODO: 基本账号注册完成后应当提示`您已完成账号注册, 请继续完成信息填写!`
+        // TODO: 如果用户填写的账号和密码经过后台效验是有效的, 并且该用户出于未完成必填信息阶段, 那么将可以进行下一步
+
+        // TODO: 密码输入是否一致判断
+        // TODO: 手机号是否合法判断
+        if (password !== conformPassword) {
+          Toast.show('两次输入手机号不一致 ' + telephone + ' ' + conformPassword)
+          return
+        }
+        patientRegister(telephone, password)
+          //TODO: 应当在http.ts加入判断, 如果code 不等于2000那么应当抛出错误, 以便这里的catch可以接收
+          .then((resp) => {
+            Toast.show('基本账号注册成功')
+            setCurStep(curStep + 1)
+          })
+          .catch((error) => {
+            Toast.show(error)
+          })
+        break
+      case 1:
+        setCurStep(curStep + 1)
+        break
+      case 2:
+        break
+    }
+  }
+  const register = () => {
+    patientRegister("18888888892", "123456")
+      .then((resp) => {
+        console.log(resp.data)
+        console.log(resp)
+      })
+      .catch((error: AxiosError) => {
+        console.log(error)
+      })
+  }
+  return (
+    <div>
+      <Steps current={curStep}>
+        <Step title={`账号`}/>
+        <Step title={`主要信息`}/>
+        <Step title={`其他信息`} description={`选填`}/>
+      </Steps>
+      <Form
+        layout={`horizontal`}
+        footer={
+          <Button color={`primary`} block={true} type={`submit`}
+                  fill={`outline`}>{curStep < 2 ? '下一步' : '提交'}</Button>
+        }
+        onFinish={clickNextStep}
+        onFinishFailed={() => {
+          Toast.show('onFinishFailed')
+        }}
+      >
+        <Form.Item
+          name={`telephone`}
+          label={`手机号`}
+          rules={[{required: true, message: '手机号不能为空'}]}
+          initialValue={telephone}
+        >
+          <Input onChange={val => setTelephone(val)} placeholder={`请输入手机号`}/>
+        </Form.Item>
+        <Form.Item
+          name={`password`}
+          label={`密码`}
+          rules={[{required: true, message: '密码不能为空'}]}
+        >
+          <Input onChange={val => setPassword(val)} placeholder={`请输入密码`} clearable={true} type={`password`}/>
+        </Form.Item>
+        <Form.Item
+          name={`conformPassword`}
+          label={`确认密码`}
+          rules={[{required: true, message: '确认密码不能为空'}]}
+        >
+          <Input onChange={val => setConformPassword(val)} placeholder={`请再次输入密码`} clearable={true} type={`password`}/>
+        </Form.Item>
+      </Form>
+
+    </div>
+  )
+}
+
+export default Register
