558eecbe6e229deee0f21cc862bcb7b92f30b268
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-07-15 18:26:36 +0800

    feat($api): patient login api

---
+++ src/api/v1/api/patient/login.ts
@@ -0,0 +1,19 @@
+import {patientHttp} from "./patient";
+
+//TODO: 应当对请求数据进行规范
+interface PatientLoginRequest {
+  mobile: string,
+  pd: string
+}
+
+export const patientLogin = (telephone: string, password: string) => {
+  //TODO: 应当data传入JSON格式, 然后在某处自动将其转换为FormData对象并将data替换
+  let formData = new FormData()
+  formData.append('mobile', telephone)
+  formData.append('pd', password)
+  return patientHttp({
+    url: '/login',
+    data: formData,
+    method: 'POST',
+  })
+}
