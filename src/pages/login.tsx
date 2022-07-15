f9affcb1fbe4352d1dca66bff1d114705eb3b84d
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-07-15 18:27:06 +0800

    feat($page): basic patient login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import {AutoCenter, Button, Form, Grid, Input, Toast} from "antd-mobile";
 import {useRouter} from "next/router";
 import styles from "../css/pages/login.module.scss"
+import {patientLogin} from "../api/v1/api/patient/login";
 
 // TODO: 应当抽象公共逻辑, 使得该页面患者和医生登录都共用一个页面
 const Login: React.FC<any> = () => {
@@ -11,7 +12,36 @@ const Login: React.FC<any> = () => {
   const [password, setPassword] = useState('')
 
   const clickLogin = () => {
+    patientLogin(telephone, password)
+      .then((resp) => {
+        debugger
+        const data = resp.data.data
+        if (data.loginFlag === undefined || typeof data.loginFlag !== 'number') {
+          Toast.show('内部错误')
+          console.log('未获取到字段`loginFlag`, 或者字段格式错误')
+          return
+        }
+        /**
+         * 0 信息未完善, 需要跳转到信息完善页面继续注册
+         * 1 信息完善, 可以继续业务逻辑
+         */
+        const loginFlag = data.loginFlag as number
+        if (loginFlag === 0) {
+          Toast.show('您的账号正确, 请继续注册, 完善必填信息')
+          router.push({
+            pathname: '/register',
+            query: {
+              isCompleteAccountRegister: true
+              //TODO: 应当把Token传过去
+            }
+          })
+          return
+        }
 
+        // TODO: 登录正常, 应当将用户信息存入cookie
+        // TODO: 还没有测试'登录成功'逻辑是否能够正确执行
+        Toast.show('登录成功, 你好' + data.name)
+      })
   }
   const clickRegister = () => {
     router.push({
@@ -31,7 +61,25 @@ const Login: React.FC<any> = () => {
   return (
     <div>
       <AutoCenter><h2>患者登录</h2></AutoCenter>
-      <Form layout={`horizontal`} requiredMarkStyle='text-optional' className={styles.form}>
+      <Form
+        layout={`horizontal`}
+        requiredMarkStyle='text-optional'
+        className={styles.form}
+        onFinish={clickLogin}
+        onFinishFailed={() => {
+          Toast.show('onFinishFailed')
+        }}
+        footer={
+          <Button
+            type={`submit`}
+            className={styles.loginButton}
+            block={true}
+            size={`large`}
+            color={`primary`}
+          >登录
+          </Button>
+        }
+      >
         <Form.Item
           name={`telephone`}
           label={`手机号`}
@@ -56,14 +104,6 @@ const Login: React.FC<any> = () => {
       {/*  </Button>*/}
       {/*</AutoCenter>*/}
       {/*TODO: 将Button移至From的字段属性的footer中, 以便可以使用其检测表单是否合法的特性*/}
-      <Button
-        className={styles.loginButton}
-        block={true}
-        size={`large`}
-        color={`primary`}
-        onClick={clickLogin}
-      >登录
-      </Button>
       <Grid columns={2}>
         <Grid.Item>
           <AutoCenter>
