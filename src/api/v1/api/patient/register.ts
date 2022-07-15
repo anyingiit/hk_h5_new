3e37b697195170392988b915bf57349d34c22011
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-07-15 17:31:10 +0800

    fix($api): fix patient register api request data type err

--- src/api/v1/api/patient/register.ts
+++ src/api/v1/api/patient/register.ts
@@ -1,18 +1,19 @@
 import {patientHttp} from "./patient";
 
+//TODO: 应当对请求数据进行规范
 interface PatientLoginRequest {
   mobile: string,
   pd: string
 }
 
 export const patientRegister = (telephone: string, password: string) => {
-  //TODO: 警告, 请求实际上早就被执行了, 所以这里面的参数根本没有传达到位
+  //TODO: 应当data传入JSON格式, 然后在某处自动将其转换为FormData对象并将data替换
+  let formData = new FormData()
+  formData.append('mobile', telephone)
+  formData.append('pd', password)
   return patientHttp({
     url: '/register',
-    data: {
-      mobile: telephone,
-      pd: password
-    } as PatientLoginRequest,
-    method: 'POST'
+    data: formData,
+    method: 'POST',
   })
 }
