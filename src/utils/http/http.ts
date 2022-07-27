c8f2b385674171f51422d183455bfe53759f2dae
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-07-27 18:25:53 +0800

    feat($http): support response special handling for not success

--- src/utils/http/http.ts
+++ src/utils/http/http.ts
@@ -100,6 +100,59 @@ class MyAxios {
         return Promise.reject(response)
       }
       if (!respData.success) {
+        // Array<{...}> 和 {...}[]的写法是一样的
+        /**
+         * 记录需要特殊处理的url, 已经对于这个url来说, 哪些后端返回的消息是需要特殊处理的
+         */
+        const specialHandling: Array<{
+          url: string,
+          msgTarget: Array<{
+            msg: string,
+            /**
+             * 当服务器返回的消息和这里记录的消息完全匹配(完全一致)的话, 就执行这个函数. 当该函数为`undefined`时代表什么都不需要执行
+             */
+            handling: (() => void) | undefined
+          }>
+        }> = [
+          {
+            url: '/login/doLogin_web_pf',
+            msgTarget: [
+              {
+                msg: '无此账号对应的患者',
+                handling: undefined
+              }
+            ]
+          }
+        ]
+        /**
+         * 遍历需要特殊处理的对象
+         */
+        for (const specialItem of specialHandling) {
+          /**
+           * 当实际请求url和记录url一致时
+           */
+          if (url === specialItem.url) {
+            /**
+             * 判断服务器返回的消息是否和匹配的一致
+             */
+            for (const target of specialItem.msgTarget) {
+              /**
+               * 如果一致则需要执行该函数(undefined则不需要执行)
+               */
+              if (respData.msg === target.msg) {
+                target.handling != undefined && target.handling()
+                /**
+                 * 执行成功则直接返回, 这里屏蔽了后续操作
+                 * 场景举例: 当未注册用户尝试使用注册登录综合页面进行登录的时候, 服务器会返回`该用户未注册`等相关信息, 这个时候如果将该消息返回给用户是不妥的, 因为这是预期内的错误, 此时应当什么都不提示, 应当在请求回调用进行相关操作并且提示正确信息(例如: 先将页面切换为注册页面(为啥不直接注册, 因为注册用户还有其他相关信息需要填写)), 然后提示`请继续填写表单以完成注册)
+                 */
+                return Promise.reject(response)
+              }
+            }
+          }
+        }
+        /**
+         * 当上方匹配成功后, 下方代码的操作都被屏蔽了
+         */
         Toast.show('统一出错, 接口返回操作失败了, 信息: ' + respData.msg)
         return Promise.reject(response)
       }
@@ -113,10 +166,13 @@ class MyAxios {
         switch (error.response.status) {
           // TODO: 响应错误码处理
           case 403:
+            Toast.show('403了, 请联系管理员处理')
             break
           case 404:
-            // console.log('404了')
+            Toast.show('接口未找到, 请联系管理员处理')
             break
+          default:
+            Toast.show(`出错了, status${error.response.status} 请联系管理员处理!`)
         }
         return Promise.reject(error.response)
       }
