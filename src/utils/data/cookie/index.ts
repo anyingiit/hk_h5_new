d148bb31d378c0956e8a1a51b0bdbdce5fea62cb
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-08-02 15:48:17 +0800

    feat($cookie/data): rename file `login.ts` to `loginInfo.ts`

--- src/utils/data/cookie/index.ts
+++ src/utils/data/cookie/index.ts
@@ -1,5 +1,5 @@
 // export {cookie} from './cookie'
 // export {default as userLoginState} from './modules/login' // 导入某个模块的默认导出, 并且重命名
 
-export * from './modules/login'
+export * from './modules/loginInfo'
 export {cookie} from "./cookie";
