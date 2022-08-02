6a30b56f25f6387050dc02947fa442a43c39a5cf
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-08-02 15:19:37 +0800

    fix($page/home): round cannot center

--- src/pages/main/home.tsx
+++ src/pages/main/home.tsx
@@ -6,26 +6,23 @@ const Home: React.FC = () => {
   return (
     <HomeBase
       activeKey={`home`}
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
     >
-      <div className={styles.round}>
-        <div className={styles.box}>
-          {/*术后随访*/}
-          <div className={styles.a}></div>
-          {/*健康档案*/}
-          <div className={styles.b}></div>
-          {/*正念康养*/}
-          <div className={styles.c}></div>
-          {/*我的咨询*/}
-          <div className={styles.d}></div>
-          {/*快速匹配*/}
-          <div className={styles.e}></div>
-          {/*食药营养*/}
-          <div className={styles.f}></div>
+      <div className={styles.container}>
+        <div className={styles.round}>
+          <div className={styles.box}>
+            {/*术后随访*/}
+            <div className={styles.a}></div>
+            {/*健康档案*/}
+            <div className={styles.b}></div>
+            {/*正念康养*/}
+            <div className={styles.c}></div>
+            {/*我的咨询*/}
+            <div className={styles.d}></div>
+            {/*快速匹配*/}
+            <div className={styles.e}></div>
+            {/*食药营养*/}
+            <div className={styles.f}></div>
+          </div>
         </div>
       </div>
     </HomeBase>
