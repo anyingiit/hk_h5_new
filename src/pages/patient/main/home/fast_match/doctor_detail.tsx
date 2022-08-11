bdda4b2dbec17a81b51f7fea4e0184ce0a2d613b
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-08-11 22:44:15 +0800

    feat($pages/doctor_detail): query support `id` field

--- src/pages/patient/main/home/fast_match/doctor_detail.tsx
+++ src/pages/patient/main/home/fast_match/doctor_detail.tsx
@@ -4,6 +4,12 @@ import PageBase from "../../../../../components/pageBase";
 import styles
   from "../../../../../css/pages/patient/main/home/fast_match/doctor_detail.module.scss"
 import {Avatar, Button} from "antd-mobile";
+import {isNumber, toNumber} from "../../../../../utils";
+
+
+interface Query {
+  id?: string
+}
 
 interface Props {
   id: number,
@@ -41,11 +47,47 @@ export const Doctor_detail: React.FC<Props> = ({id, name, description}) => {
 export default Doctor_detail
 
 export const getServerSideProps: GetServerSideProps = async (context): Promise<{ props: Props }> => {
-  return {
-    props: {
+  const dataBase = [
+    {
       id: 1,
       name: '何医生',
       description: '广东省英德市人民医院中医科何文星医生，中医世家，医学硕士'
+    },
+    {
+      id: 2,
+      name: '王医生',
+      description: '广东省英德市人民医院中医科何文星医生，中医世家，医学硕士'
+    },
+    {
+      id: 3,
+      name: '刘医生',
+      description: '广东省英德市人民医院中医科何文星医生，中医世家，医学硕士'
+    },
+    {
+      id: 4,
+      name: '薛医生',
+      description: '广东省英德市人民医院中医科何文星医生，中医世家，医学硕士'
+    },
+  ]
+
+  const {id}: Query = context.query
+
+
+  let props = (() => {
+    if (id === undefined || !isNumber(id)) {
+      return dataBase[0]
+    }
+    const idNumber = toNumber(id)
+
+    return dataBase.find((item) => {
+      if (item.id === idNumber) {
+        return true
+      }
+    }) ?? dataBase[0]
+  })()
+  return {
+    props: {
+      ...props
     }
   }
 }
